081329f1400df3b02089d696d492d6d7a89b2a1e
contidavide-sudo <conti.davide@franchettisalviani.net>
2025-11-18 22:46:42 +0100

    Aggiungere i file di progetto.

---
+++ normalizza/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace normalizza
+{
+    internal class Program
+    {
+        static float[] Normalizza(float[] m)
+        {
+            float[] result = new float[m.Length];
+
+            for (int i = 0; i < m.Length; i++) {
+
+                if (i == 0)
+                {
+                    result[0] = (m[0] + m[0] + m[1]) / 3;
+
+                }
+                else if (i == m.Length - 1) {
+
+                    result[m.Length - 1] = (m[m.Length - 1] + m[m.Length - 1] + m[m.Length - 2]) / 3;
+
+                }
+                else
+                {
+                    result[i] = (m[i] + m[i + 1] + m[i - 1]) / 3;
+                }
+               
+            }
+
+            return result;
+
+        }
+        static void Main(string[] args)
+        {
+            float[] vet1 = { 4, 5, 2, 8, 5 };
+
+            float[] vet2 = Normalizza(vet1);
+
+            for (int i = 0; i < vet2.Length; i++) {
+
+                Console.Write("[" + vet2[i] + "]");
+            
+            }
+
+        }
+    }
+}
